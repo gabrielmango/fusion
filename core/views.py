ad902d0a0a5712c944e65051a1e320d8d713b539
gabriel.mango <gabriel.mango@defensoria.mg.def.br>
2024-08-08 10:51:27 -0300

    implementando formulario de contato

--- core/views.py
+++ core/views.py
@@ -1,12 +1,26 @@
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 from .models import Servico, Funcionario
+from .forms import ContatoForm
 
-class IndexView(TemplateView):
+class IndexView(FormView):
     template_name = 'index.html'
+    form_class = ContatoForm
+    success_url = reverse_lazy('index')
 
     def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         context['servicos'] = Servico.objects.order_by('?').all()
         context['funcionarios'] = Funcionario.objects.order_by('?').all()
         return context
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'Mensagem enviada com sucesso!')
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        messages.error(self.request, 'Houve um erro ao enviar a mensagem.')
+        return super().form_invalid(form)
